9c539a5ea6443d82674f5f099da691c932bc0f24
MikkeGF <n9pemi00students.oamk.fi>
2021-02-01 15:39:48 +0200

    installed expo debendencies

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './HomeScreen';
 import SecondScreen from './SecondScreen';
